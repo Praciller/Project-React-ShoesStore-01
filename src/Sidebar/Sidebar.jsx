41dfc517f63c452c6415dd4fd0c056e46358c6db
Praciller <pakon1997@gmail.com>
2023-07-27 20:56:13 +0700

    first commit

---
+++ src/Sidebar/Sidebar.jsx
@@ -0,0 +1,23 @@
+import Category from "./Category/Category";
+import Price from "./Price/Price";
+import Colors from "./Colors/Colors";
+import "./Sidebar.css";
+import Logo from "../Images/P-Logo.svg"
+
+
+const Sidebar = ({ handleChange }) => {
+  return (
+    <>
+      <section className="sidebar">
+        <div className="logo-container">
+          <img src={Logo} alt="" />
+        </div>
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
+      </section>
+    </>
+  );
+};
+
+export default Sidebar;
